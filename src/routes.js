22cfdd812ad119c57f34ae65d10a388546c1222c
Samba SG <sougousambaofficiel@gmail.com>
2019-11-02 18:44:16 +0000

    removed some unused files

--- src/routes.js
+++ src/routes.js
@@ -15,14 +15,7 @@ const UIBasicCollapse = React.lazy(() => import('./ComponentLib/UIElements/Basic
 const UIBasicTabsPills = React.lazy(() => import('./ComponentLib/UIElements/Basic/TabsPills'));
 const UIBasicBasicTypography = React.lazy(() => import('./ComponentLib/UIElements/Basic/Typography'));
 
-const FormsElements = React.lazy(() => import('./ComponentLib/Forms/FormsElements'));
-
-const BootstrapTable = React.lazy(() => import('./ComponentLib/Tables/BootstrapTable'));
-
 const Nvd3Chart = React.lazy(() => import('./ComponentLib/Charts/Nvd3Chart/index'));
-
-const OtherSamplePage = React.lazy(() => import('./ComponentLib/Other/SamplePage'));
-const OtherDocs = React.lazy(() => import('./ComponentLib/Other/Docs'));
 
 //My imports 
 const ProjectOverview = React.lazy(() => import('./ComponentLib/Dashboard/Projects'));
@@ -38,11 +31,8 @@ const routes = [
     { path: '/basic/collapse', exact: true, name: 'Basic Collapse', component: UIBasicCollapse },
     { path: '/basic/tabs-pills', exact: true, name: 'Basic Tabs & Pills', component: UIBasicTabsPills },
     { path: '/basic/typography', exact: true, name: 'Basic Typography', component: UIBasicBasicTypography },
-    { path: '/forms/form-basic', exact: true, name: 'Forms Elements', component: FormsElements },
-    { path: '/tables/bootstrap', exact: true, name: 'Bootstrap Table', component: BootstrapTable },
     { path: '/charts/nvd3', exact: true, name: 'Nvd3 Chart', component: Nvd3Chart },
-    { path: '/sample-page', exact: true, name: 'Sample Page', component: OtherSamplePage },
-    { path: '/docs', exact: true, name: 'Documentation', component: OtherDocs },
+
     // My routes
     { path: '/dashboard', exact: true, name: 'Dashboard', component: Dashboard },
     { path: '/projets', exact: true, name: 'Projets', component: ProjectOverview },
